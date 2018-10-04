214034566200579c079d9ac302f2192501348ad8
Kacper Mazur <33032053+kacpermazur@users.noreply.github.com>
2018-10-04 11:56:24 +0100

    Session 1 Update 1.0
    ...

---
+++ Pratical Sessions/Pratical One/Pratical 1/src/pratical/pkg1/MyWeight.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratical.pkg1;
+
+/**
+ *
+ * @author kmazur
+ */
+public class MyWeight {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    int kg = 10;
+    
+    double pounds = kg * 2.2;
+    
+    System.out.println(pounds);
+    
+    }
+    
+}
---
+++ Pratical Sessions/Pratical One/Pratical 1/src/pratical/pkg1/Pratical1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratical.pkg1;
+
+/**
+ *
+ * @author kmazur
+ */
+public class Pratical1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int day = 22;
+        
+        System.out.println("hello");
+        System.out.println("The date today is " + day);
+        System.out.println(true);
+        
+        
+        System.exit(0);
+    }
+    
+}
